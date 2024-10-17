5c3322ec41167f7488a6a48500931f9bbb6eafed
Kyoko Akatsuka <akatsuka.kyoko@plus-zero.co.jp>
2024-10-17 19:23:33 +0900

    Login.tsx追加

---
+++ src/Login.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+interface LoginProps {
+  onSubmit: (email: string, password: string) => void;
+}
+
+const Login: React.FC<LoginProps> = ({ onSubmit }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit(email, password);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>ログイン</h2>
+        <label>
+          メールアドレス
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          パスワード
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">ログイン</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
